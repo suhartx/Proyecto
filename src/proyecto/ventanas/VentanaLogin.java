9514e62ddc87dd2c6e58e8c345fb9f92cb2c17bd
suhar <suhartx@opendeusto.es>
2020-10-21 16:06:25 +0200

    Ventanas casi creadas. próximo objetivo personalizarlas un poco mas y ir creando las clases de usuario e ir creando la base de datos para implementar en el loggin la lista de usuarios y que abra una ventana u otra segun el tipo de usuario que seas

--- src/proyecto/ventanas/VentanaLogin.java
+++ src/proyecto/ventanas/VentanaLogin.java
@@ -14,18 +14,25 @@ import javax.swing.border.EmptyBorder;
  * Facultad de Ingeniería - Universidad de Deusto
  */
 
+
+
 public class VentanaLogin extends JFrame {
 	
 	// Varible de ventana de logueo de la clase
     private JPanel contenido;
     static Thread hiloActual;
+    
+    private JTextField userText;
+    private JPasswordField passwordText;
+    
+    
 
     public VentanaLogin() {
 
     	// Creación de componentes/contenedores de swing
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle( "Ventana Login" );
-        setSize( 800, 600 );
+        setSize( 400, 350 );
         setLocationRelativeTo( null );
         ImageIcon icon = new ImageIcon("src\\imagenes\\osakidetza.png");
         setIconImage(icon.getImage());
@@ -34,15 +41,19 @@ public class VentanaLogin extends JFrame {
         contenido = new JPanel();
         contenido.setBorder(new EmptyBorder(5, 5, 5, 5));
         contenido.setLayout(new BorderLayout(0, 0));
+        contenido.setBackground(new java.awt.Color(1, 116, 255));
+        contenido.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Acceder", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+        		javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Arial Narrow", 0, 25), new java.awt.Color(255, 255, 255)));
         setContentPane(contenido);
         
         contenido.setLayout(null);
         
         JLabel userLabel = new JLabel("Usuario:");
 		userLabel.setBounds(50, 100, 80, 25);
+		//userLabel.setBounds((int)(getBounds().width*0.2),(int)(getBounds().height*0.3),80, 25);//mirar como crear contenido dinamico
 		contenido.add(userLabel);
 		
-		JTextField userText = new JTextField(20);
+		userText = new JTextField(20);
 		userText.setBounds(140, 100, 160, 25);
 		contenido.add(userText);
 		
@@ -50,7 +61,7 @@ public class VentanaLogin extends JFrame {
 		passwordLabel.setBounds(50, 150, 80, 25);
 		contenido.add(passwordLabel);
 
-		JPasswordField passwordText = new JPasswordField(20);
+		passwordText = new JPasswordField(20);
 		passwordText.setBounds(140, 150, 160, 25);
 		contenido.add(passwordText);
 		
@@ -65,13 +76,34 @@ public class VentanaLogin extends JFrame {
 		loginButton.addActionListener(new ActionListener() {
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-		//		JOptionPane.showMessageDialog(null, "login button has been pressed");
-				//hiloActual.interrupt();
+			public void actionPerformed(ActionEvent evt) {
+
+
+				loginButtonActionPerformed(evt);
+				
 			}
 		});
 		
+    }
+    
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        String usuario = userText.getText();
+        String paswd = passwordText.getText();
+        
+        if(usuario.isEmpty() || paswd.isEmpty()){
+            JOptionPane.showMessageDialog(null, "AlgÃºn campo esta vacio");
+            
+        }else{
+             if(usuario.equals("usuario1") && paswd.equals("1234")){
+                 JOptionPane.showMessageDialog(null,"Bienvenido");
+                 VentanaMedico pc = new VentanaMedico();
+                 pc.setVisible(true);
+                 this.dispose();
+                 
+             }else{
+                 JOptionPane.showConfirmDialog(null,"Su usuario o contraseÃ±a es incorrecto");
+             }
+        }
     }
     
     
